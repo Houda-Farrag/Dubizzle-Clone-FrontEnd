306bc937ab43e9eb3273521edfb1bae73e423551
Houda-Faraag <mahmiti2022@gmail.com>
2024-02-28 14:45:22 +0200

    ''

--- src/Pages/Property/Property.jsx
+++ src/Pages/Property/Property.jsx
@@ -1,24 +1,27 @@
-
 import FeaturedAgencies from '../../Components/FeaturedAgencies/FeaturedAgencies';
+import Footer from '../../Components/FooterComp/Footer';
+import Header from '../../Components/HeaderComp/Header';
 import PropertyForm from '../../Components/PropertyForm/PropertyForm';
 import styles from './Property.module.css'
 // import banner from '../../assets/images/propertiesVerticalBanner.png'
 const Property = () => {
     return (
-            <>
-            <div className='container'> 
-            <div className={styles.banner} style={{marginBottom:'50px'}}> 
-                <div className='z-50 mb-5'>
-                    <h1 className='font-bold mb-2' style={{fontSize:'39px'}}>Your next property is here.</h1>
-                    <p className='text-2xl'>Let&apos;s find a home that&apos;s perfect for you</p>
+        <>
+            <Header></Header>
+            <div className='container mt-24'>
+                <div className={styles.banner} style={{ marginBottom: '50px' }}>
+                    <div className='z-50 mb-5'>
+                        <h1 className='font-bold mb-2' style={{ fontSize: '39px' }}>Your next property is here.</h1>
+                        <p className='text-2xl'>Let&apos;s find a home that&apos;s perfect for you</p>
+                    </div>
+                    <PropertyForm />
                 </div>
-                <PropertyForm/>
+                <div>
+                    <FeaturedAgencies />
+                </div>
             </div>
-            <div>
-                <FeaturedAgencies/>
-            </div>
-            </div>
-</>
+            <Footer></Footer>
+        </>
     );
 }
 
