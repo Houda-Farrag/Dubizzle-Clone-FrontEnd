ce72cb0cc1b6b85d1abe7c71618e825aafb337d1
MinaE.Seiffen <minaseiffen@gmail.com>
2024-03-14 02:08:41 +0200

    breadcrumb and sell form updates

--- src/Components/BreadCrumb/BreadCrumb.jsx
+++ src/Components/BreadCrumb/BreadCrumb.jsx
@@ -8,7 +8,7 @@ export const BreadCrumb = () => {
 
 
   return (
-    <nav className="flex mx-28 my-10 w-fit" aria-label="Breadcrumb">
+    <nav className="flex mx-20 my-10 w-fit" aria-label="Breadcrumb">
       <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
         <li className="inline-flex items-center">
           <a
--- src/Components/SellForm/SellForm.jsx
+++ src/Components/SellForm/SellForm.jsx
@@ -1,6 +1,6 @@
 import upload_area from "../../assets/upload_area.svg";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BreadCrumb } from "../BreadCrumb/BreadCrumb";
 import { Link } from "react-router-dom";
 import Select from "react-select";
@@ -9,19 +9,24 @@ import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { useMenuSelectionContext } from "../../Context/MenuSelectionContext";
 
 const SellForm = () => {
-  const {
-    selectedMenuItem,
-    selectedSubMenu1,
-  } = useMenuSelectionContext();
+  const { selectedCategory, selectedSubCategory } = useMenuSelectionContext();
   const [negotiable, setNegotiable] = useState(false);
   const [exchange, setExchange] = useState(false);
   const [free, setFree] = useState(false);
+  const [duplex, setDuplex] = useState(false);
+  const [townHouse, setTownHouse] = useState(false);
+  const [standAlone, setStandAlone] = useState(false);
+  const [balcony, setBalcony] = useState(false);
+  const [kitchen, setKitchen] = useState(false);
+  const [garden, setGarden] = useState(false);
+  const [security, setSecurity] = useState(false);
   const [image, setImage] = useState(false);
   const [contactMethod, setContactMethod] = useState([]);
   const [selectedBrand, setSelectedBrand] = useState(null);
   const [isDropdownLocation, setisDropdownLocation] = useState(false);
   const [selectedLocation, setSelectedLocation] = useState("");
   const [priceType, setPriceType] = useState("");
+  const [isproperty, setIsProperty] = useState(false);
   const [productDetails, setProductDetails] = useState({
     name: "",
     description: "",
@@ -70,7 +75,13 @@ const SellForm = () => {
 
   const addProduct = async () => {
     let responseData;
-    let product = { ...productDetails, price_type: priceType, category: selectedMenuItem, subCategory: selectedSubMenu1, contact_type: contactMethod };
+    let product = {
+      ...productDetails,
+      price_type: priceType,
+      category: selectedCategory,
+      subCategory: selectedSubCategory,
+      contact_type: contactMethod,
+    };
     let formData = new FormData();
     formData.append("product", image);
 
@@ -140,6 +151,39 @@ const SellForm = () => {
     setFree(true);
     setPriceType("free");
   };
+  const handleDuplex = () => {
+    setDuplex(true);
+    setTownHouse(false);
+    setStandAlone(false);
+    setPriceType("Duplex");
+  };
+
+  const handleTownHouse = () => {
+    setDuplex(false);
+    setTownHouse(true);
+    setStandAlone(false);
+    setPriceType("Town House");
+  };
+
+  const handleStandAlone = () => {
+    setDuplex(false);
+    setTownHouse(false);
+    setStandAlone(true);
+    setPriceType("Stand Alone");
+  };
+
+  const handleBalcony = () => {
+    setBalcony(!balcony);
+  };
+  const handlekitchen = () => {
+    setKitchen(!kitchen);
+  };
+  const handleSecurity = () => {
+    setSecurity(!security);
+  };
+  const handleGarden = () => {
+    setGarden(!garden);
+  };
 
   const handleContactMethodChange = (method) => {
     if (method === "phone") {
@@ -166,8 +210,20 @@ const SellForm = () => {
     await addProduct();
   };
 
+  useEffect(() => {
+    const isPropertySelected = async () => {
+      if (selectedCategory == "Properties") {
+        setIsProperty(true);
+      }
+    };
+    isPropertySelected();
+  }, [setIsProperty, selectedCategory]);
+
   return (
     <>
+      <h1 className="font-semibold py-4 text-3xl mx-auto flex justify-center">
+        Post AD
+      </h1>
       <div className="container mx-auto border p-1">
         <div className="flex items-center">
           <BreadCrumb />
@@ -178,12 +234,13 @@ const SellForm = () => {
           </Link>
           <hr className="w-fit" />
         </div>
-        <div className="max-w-3xl mx-28">
-          <h1 className="font-bold text-lg mb-3">Includes Some Details</h1>
+        <hr className="w-full mb-2" />
+        <div className="mx-20">
+          <h1 className="font-semibold text-2xl mb-4">Includes Some Details</h1>
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
-              <label htmlFor="productName" className="block mb-1">
-                Product Name:
+              <label htmlFor="productName" className="text-md block mb-1">
+                AD Title:
               </label>
               <input
                 type="text"
@@ -194,22 +251,158 @@ const SellForm = () => {
                 className="w-full border rounded px-3 py-2"
                 required
               />
+              <p className="text-sm text-black font-normal">
+                Please, Mention the key features of your item
+              </p>
             </div>
+            {!isproperty && (
+              <div className="mb-4">
+                <label htmlFor="brand" className="block mb-1">
+                  Brand:
+                </label>
+                <Select
+                  options={brands}
+                  value={selectedBrand}
+                  onChange={(selectedOption) =>
+                    setSelectedBrand(selectedOption)
+                  }
+                  placeholder="Select a brand"
+                  isSearchable
+                  className="w-full bg-gray-50"
+                  required
+                />
+              </div>
+            )}
+            {isproperty && (
+              <div className="mb-4">
+                <label className="text-md mb-1">Property Type:</label>
+                <div className="mb-4 flex gap-5 m-3">
+                  <div className="p-2 border w-fit flex justify-center items-center">
+                    <input
+                      type="checkbox"
+                      checked={duplex}
+                      onChange={handleDuplex}
+                      className="circle bg-white appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500 checked:border-transparent"
+                    />
+                    <label htmlFor="negotiable" className="mx-3 relative top-1">
+                      Duplex
+                    </label>
+                  </div>
+                  <div className="p-2 border w-fit flex justify-center items-center">
+                    <input
+                      type="checkbox"
+                      id="exchange"
+                      checked={standAlone}
+                      onChange={handleStandAlone}
+                      className="circle appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500 checked:border-transparent"
+                    />
+                    <label htmlFor="exchange" className="mx-3 relative top-1">
+                      Stand Alone
+                    </label>
+                  </div>
+                  <div className="p-2 border w-fit flex justify-center items-center">
+                    <input
+                      type="checkbox"
+                      id="free"
+                      checked={townHouse}
+                      onChange={handleTownHouse}
+                      className="circle bg-white appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500"
+                    />
+                    <label htmlFor="free" className="mx-3 relative top-1">
+                      Town House
+                    </label>
+                  </div>
+                </div>
+              </div>
+            )}
+            {isproperty && (
+              <div className="mb-4">
+                <label>
+                  Area (m<span className="relative text-sm bottom-1">2</span>):
+                </label>
+                <input
+                  type="number"
+                  name="area"
+                  className="w-full border rounded px-3 py-2 mt-3"
+                />
+              </div>
+            )}
 
-            <div className="mb-4">
-              <label htmlFor="brand" className="block mb-1">
-                Brand:
-              </label>
-              <Select
-                options={brands}
-                value={selectedBrand}
-                onChange={(selectedOption) => setSelectedBrand(selectedOption)}
-                placeholder="Select a brand"
-                isSearchable
-                className="w-full bg-gray-50"
-                required
-              />
-            </div>
+            {isproperty && (
+              <div>
+                <label className="block mb-1">Amenities:</label>
+                <div className="mb-4 flex gap-5 m-3">
+                  <div className="p-2 border w-fit flex justify-center items-center">
+                    <input
+                      type="checkbox"
+                      checked={balcony}
+                      onChange={handleBalcony}
+                      className="circle bg-white appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500 checked:border-transparent"
+                    />
+                    <label htmlFor="negotiable" className="mx-3 relative top-1">
+                      Balcony
+                    </label>
+                  </div>
+                  <div className="p-2 border w-fit flex justify-center items-center">
+                    <input
+                      type="checkbox"
+                      id="exchange"
+                      checked={kitchen}
+                      onChange={handlekitchen}
+                      className="circle appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500 checked:border-transparent"
+                    />
+                    <label htmlFor="exchange" className="mx-3 relative top-1">
+                      Built in Kitchen Appliances
+                    </label>
+                  </div>
+                  <div className="p-2 border w-fit flex justify-center items-center">
+                    <input
+                      type="checkbox"
+                      id="free"
+                      checked={garden}
+                      onChange={handleGarden}
+                      className="circle bg-white appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500"
+                    />
+                    <label htmlFor="free" className="mx-3 relative top-1">
+                      Private Garden
+                    </label>
+                  </div>
+                  <div className="p-2 border w-fit flex justify-center items-center">
+                    <input
+                      type="checkbox"
+                      id="free"
+                      checked={security}
+                      onChange={handleSecurity}
+                      className="circle bg-white appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500"
+                    />
+                    <label htmlFor="free" className="mx-3 relative top-1">
+                      Security
+                    </label>
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {isproperty && (
+              <div className="mb-4">
+                <div className="mb-4">
+                  <label>Bedrooms:</label>
+                  <input
+                    type="number"
+                    name="bedrooms"
+                    className="w-full border rounded px-3 py-2 mt-3"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label>Bathrooms:</label>
+                  <input
+                    type="number"
+                    name="bathrooms"
+                    className="w-full border rounded px-3 py-2 mt-3"
+                  />
+                </div>
+              </div>
+            )}
 
             <div className="mb-4">
               <label htmlFor="description" className="block mb-1">
@@ -224,12 +417,20 @@ const SellForm = () => {
                 rows="4"
                 required
               ></textarea>
+              <p className="text-sm">
+                Please,Include condition, features and reason for selling
+              </p>
             </div>
+            <hr className="w-full" />
 
             <div className="relative mb-4">
-              <label htmlFor="price" className="block mb-1">
+              <label
+                htmlFor="price"
+                className="block mb-7 mt-5 text-2xl font-bold"
+              >
                 Set a Price:
               </label>
+              <label className="block mb-2">Price:</label>
               <div className="relative">
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600 pointer-events-none">
                   EGP
@@ -246,37 +447,48 @@ const SellForm = () => {
               </div>
             </div>
 
-            <div className="mb-4">
-              <input
-                type="checkbox"
-                id="negotiable"
-                checked={negotiable}
-                onChange={handleNegotiableChange}
-                className="circle appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500 checked:border-transparent"
-              />
-              <label htmlFor="negotiable" className="mx-3 relative top-1">
-                Negotiable
-              </label>
-              <input
-                type="checkbox"
-                id="exchange"
-                checked={exchange}
-                onChange={handleExchangeChange}
-                className="circle appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500 checked:border-transparent"
-              />
-              <label htmlFor="exchange" className="mx-3 relative top-1">
-                Exchange
-              </label>
-              <input
-                type="checkbox"
-                id="free"
-                checked={free}
-                onChange={handleFreeChange}
-                className="circle appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500 checked:border-transparent"
-              />
-              <label htmlFor="free" className="mx-3 relative top-1">
-                Free
-              </label>
+            <div className="mb-4 flex gap-5">
+              <div className="p-2 border w-fit flex justify-center items-center">
+                <input
+                  type="checkbox"
+                  id="negotiable"
+                  checked={negotiable}
+                  onChange={handleNegotiableChange}
+                  className="circle bg-white appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500 checked:border-transparent"
+                />
+                <label htmlFor="negotiable" className="mx-3 relative top-1">
+                  Negotiable
+                </label>
+              </div>
+              {!isproperty && (
+
+              <div className="p-2 border w-fit flex justify-center items-center">
+                <input
+                  type="checkbox"
+                  id="exchange"
+                  checked={exchange}
+                  onChange={handleExchangeChange}
+                  className="circle appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500 checked:border-transparent"
+                />
+                <label htmlFor="exchange" className="mx-3 relative top-1">
+                  Exchange
+                </label>
+              </div>
+              )}
+              {!isproperty && (
+              <div className="p-2 border w-fit flex justify-center items-center">
+                <input
+                  type="checkbox"
+                  id="free"
+                  checked={free}
+                  onChange={handleFreeChange}
+                  className="circle bg-white appearance-none border border-black rounded-full w-4 h-4 checked:bg-red-500"
+                />
+                <label htmlFor="free" className="mx-3 relative top-1">
+                  Free
+                </label>
+              </div>
+              )}
             </div>
 
             <div className="add-product-itemfield">
@@ -295,9 +507,9 @@ const SellForm = () => {
                 hidden
               />
             </div>
-
-            <div className="mb-4">
-              <label htmlFor="location" className="block mb-1">
+              <hr className="w-full mt-3"/>
+            <div className="mb-6">
+              <label htmlFor="location" className="block mb-6 mt-5 text-2xl font-bold">
                 Location:
               </label>
               <div className="relative border p-4">
@@ -312,10 +524,11 @@ const SellForm = () => {
                     {selectedLocation || "Select Location"}
                   </span>
                   <svg
-                    className={`text-black ml-2 h-5 w-5 group-hover:text-black ${isDropdownLocation
+                    className={`text-black ml-2 h-5 w-5 group-hover:text-black ${
+                      isDropdownLocation
                         ? "rotate-180 duration-500"
                         : "duration-500"
-                      }`}
+                    }`}
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
@@ -380,7 +593,9 @@ const SellForm = () => {
                 className="border rounded px-3 py-2 w-full mt-2"
                 required
               />
-              <div className="mt-2">
+              <div className="mt-2 gap-5 flex">
+                <label className="text-lg">Contact Type:</label>
+                <div className="flex justify-center items-center w-fit border">
                 <input
                   type="checkbox"
                   id="phone"
@@ -392,6 +607,8 @@ const SellForm = () => {
                 <label htmlFor="phone" className="mx-3 relative top-1">
                   Phone
                 </label>
+                </div>
+                <div className="flex justify-center items-center w-fit border">
                 <input
                   type="checkbox"
                   id="chat"
@@ -403,6 +620,8 @@ const SellForm = () => {
                 <label htmlFor="chat" className="mx-3 relative top-1">
                   Chat
                 </label>
+                </div>
+                <div className="flex justify-center items-center w-fit border">
                 <input
                   type="checkbox"
                   id="both"
@@ -414,9 +633,9 @@ const SellForm = () => {
                 <label htmlFor="both" className="mx-3 relative top-1">
                   Both
                 </label>
+                </div>
               </div>
             </div>
-
             <button
               type="submit"
               className="bg-red-600 font-bold mt-3 text-white px-6 py-2 rounded-lg"
